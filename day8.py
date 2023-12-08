c2095a0979d1b571ede24edb841bc67aa41f9de9
Ivy Xu <ivy21418@gmail.com>
2023-12-08 12:03:31 +0200

    the work for day8

---
+++ day8.py
@@ -0,0 +1,85 @@
+import math
+
+# parse instructions into string, network into dict
+with open("day8.txt") as file:
+	data = file.readlines()
+
+instr = data[0].rstrip()
+
+nodes = {}
+for line in data[2:]:
+	lst = line.split()
+	lst = [''.join(filter(str.isalpha, s)) for s in lst]
+	nodes[lst[0]] = (lst[2], lst[3])
+
+""" part one """
+steps = 0
+# initially, next location (the location to search for in dict) is set to "AAA"
+next_loc = "AAA"
+while True:
+	for dirct in instr:
+		for loc, dest in nodes.items():
+			if next_loc == "ZZZ":
+				break
+			if loc == next_loc:
+				steps += 1
+				if dirct == 'L':
+					next_loc = dest[0]
+				else:
+					next_loc = dest[1]
+				break
+		if next_loc == "ZZZ":
+			break
+	if next_loc == "ZZZ":
+			break
+print(f"part1: {steps}")
+
+""" part two """
+def last_char(string: str) -> str:
+	return string[-1]
+
+# search for all nodes starting with "**A" and save to a list
+start_locs = []
+for loc in nodes:
+	if last_char(loc) == 'A':
+		start_locs.append(loc)
+
+# record minimum steps required for each route ("**A" -> ... -> "**Z")
+steps_lst = []
+for start_loc in start_locs:
+	steps = 0
+	next_loc = start_loc
+	while True:
+		for dirct in instr:
+			for loc, dest in nodes.items():
+				if last_char(next_loc) == 'Z':
+					break
+				if loc == next_loc:
+					steps += 1
+					if dirct == 'L':
+						next_loc = dest[0]
+					else:
+						next_loc = dest[1]
+					break
+			if last_char(next_loc) == 'Z':
+				break
+		if last_char(next_loc) == 'Z':
+				break
+	steps_lst.append(steps)
+
+# verify if all the steps are divisible by len(instr), 
+# i.e. if all routes take full circles of instructions to finish
+lcm_usable = True
+for num in steps_lst:
+	if num % len(instr) != 0:
+		lcm_usable = False
+print(f"lcm_usable: {lcm_usable}")
+
+def lcm(a, b):
+    return abs(a * b) // math.gcd(a, b)
+
+result = steps_lst[0]
+for i in range(1, len(steps_lst)):
+	result = lcm(result, steps_lst[i])
+
+print(f"part2: {result}")
